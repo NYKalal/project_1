8138a098ffb34a39fc4c17d4bfbef068b2656790
nagaraj y <nyk6426@gmail.com>
2024-02-16 09:50:22 +0530

    logger file added

---
+++ src/mcq_generator/logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = "{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+
+# creating the directory called logs in the current working directory to populate the logs
+log_path = os.path.join(os.getcwd(),"logs")
+os.makedirs(log_path,exist_ok=True)
+
+
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(
+    level=logging.INFO, 
+    filename=LOG_FILE_PATH,
+format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+
+
+# Now I need to use this logger in my end-to-end project
+# very very important
